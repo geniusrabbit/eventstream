e39bfe0f431dc52ccc480a3e5f76455228ec6b9c
Dmitry Ponomarev <demdxx@trafficstars.com>
2019-10-15 11:48:28 +0200

    Add stream options test

---
+++ stream/options_test.go
@@ -0,0 +1,40 @@
+package stream
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_Optins(t *testing.T) {
+	var tests = []struct {
+		options []Option
+		config  Config
+	}{
+		{
+			options: []Option{WithConfig(&Config{Name: "test1"})},
+			config:  Config{Name: "test1"},
+		},
+		{
+			options: []Option{WithName("test2")},
+			config:  Config{Name: "test2"},
+		},
+		{
+			options: []Option{WithName("test3"), WithDebug(true)},
+			config:  Config{Name: "test3", Debug: true},
+		},
+		{
+			options: []Option{WithName("test4"), WithWhere("id = 100")},
+			config:  Config{Name: "test4", Where: "id = 100"},
+		},
+	}
+
+	for _, test := range tests {
+		var cnf Config
+		for _, opt := range test.options {
+			opt(&cnf)
+		}
+		if !reflect.DeepEqual(&cnf, &test.config) {
+			t.Errorf("[%s] invalid option result", cnf.Name)
+		}
+	}
+}
